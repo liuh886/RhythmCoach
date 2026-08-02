6dcc8036d2848fe462449297f62131f0543d7b6c
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-02 13:19:54 +0800

    release: rotate PWA cache for v1.0.1

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rhythmcoach-v1.0.0';
+const CACHE_NAME = 'rhythmcoach-v1.0.1';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './rhythmcoach.svg'];
 
 self.addEventListener('install', (event) => {
