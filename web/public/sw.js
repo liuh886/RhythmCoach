8573a7aed3add38b27b297d9f5d9c81afe305b19
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-16 23:23:29 +0800

    release: rotate v1.1.0 app cache

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rhythmcoach-v1.0.1';
+const CACHE_NAME = 'rhythmcoach-v1.1.0';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './rhythmcoach.svg'];
 
 self.addEventListener('install', (event) => {
